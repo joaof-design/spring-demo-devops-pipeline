785c96b94c5354bd06eb71a6cbf8e585f60b31fd
joao fernandes <joalo.fernandes@cognizant.com>
2023-01-07 12:30:11 +0000

    initial commit

---
+++ src/main/java/com/example/springdemodevopspipeline/SpringDemoDevopsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemodevopspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoDevopsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoDevopsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdemodevopspipeline/SpringDemoDevopsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemodevopspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoDevopsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
