acc52b49f0b5896da83fc23d5249baf413da8f37
joao fernandes <joalo.fernandes@cognizant.com>
2023-01-07 12:47:40 +0000

    add message controller

---
+++ src/main/java/com/example/springdemodevopspipeline/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.example.springdemodevopspipeline.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @RequestMapping("/message")
+    public String message(){
+        return "Deploy to Azure k8s cluster ";
+    }
+}
